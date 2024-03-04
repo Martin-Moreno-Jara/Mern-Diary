ef33dafa0eb6a4adb7685cdc88e02511cf7cb862
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-03-03 22:46:55 -0500

    Starting React app

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 const dbURI = process.env.MONGO_URI;
-const entryRoutes = require('./routes/entry');
+const entryRoutes = require("./routes/entry");
 
 //MIDDLEWARE
 app.use(express.json());
 
-app.use((req,res,next)=>{
-    console.log(req.path,req.method);
-    next()
-})
-
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
 //ROUTER USE
-app.use('/api/entry',entryRoutes);
+app.use("/api/entry", entryRoutes);
 
 //DB CONNECTION
-    mongoose.connect(dbURI)
-        .then(()=>{
-            app.listen(PORT,()=>{console.log(`Listening to port ${PORT} and connected to db ${dbURI}`)})
-
-        })
-        .catch((error)=>{console.log(error)})
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening to port ${PORT} and connected to db ${dbURI}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
