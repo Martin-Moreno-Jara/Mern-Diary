e89d4c8e16a27a9e8561e7cfc8957df1159cd600
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-03-21 20:02:05 -0500

    creating userModel schema

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const User = require("../models/userModel");
 //login use
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  res.json({ msg: "login user" });
+};
 
 //signup user
 
-const signupUser = async (req, res) => {};
+const signupUser = async (req, res) => {
+  res.json({ msg: "signup user" });
+};
 
 module.exports = { loginUser, signupUser };
