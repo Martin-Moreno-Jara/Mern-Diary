f49e39bfd962ea8f4dc71926a11a18547e96b591
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-03-29 12:36:27 -0500

    [feat]: login using new static function in userModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
 const jwt = require("jsonwebtoken");
+
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.JWT_STRING, { expiresIn: "3d" });
 };
 //login use
 
 const loginUser = async (req, res) => {
-  res.json({ msg: "login user" });
+  const { email, password } = req.body;
+  try {
+    const login = await User.login(email, password);
+    const token = createToken(login._id);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 //signup user
