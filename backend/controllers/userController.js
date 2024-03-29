fc54f92c62a38e493ad9e87a8a79d94be5a1d066
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2024-03-29 12:14:29 -0500

    [feat]: creating json token in signup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_STRING, { expiresIn: "3d" });
+};
 //login use
 
 const loginUser = async (req, res) => {
@@ -12,7 +16,8 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const userCreated = await User.signup(email, password);
-    res.status(200).json({ email, userCreated });
+    const token = createToken(userCreated._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
